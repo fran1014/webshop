41c25c5625d9657464b31c3e97f1bc7421646dd7
Franklin <71532004+fran1014@users.noreply.github.com>
2023-10-31 13:03:36 +0100

    adding show count functions

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsHeaderComponent implements OnInit  {
   sort= 'desc';
+  itemsShowCount=12;
+
+  
   constructor (){}
 
   ngOnInit():void{}
